02ce4c46c7a24d1110b26a82e2c404907ea0cb19
Frederik Williams <frwi@queue-it.net>
2014-09-11 11:07:12 +0200

    Released version 1.3.31 (strongly named assembly)

--- QueueIT.Security/Properties/AssemblyInfo.cs
+++ QueueIT.Security/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.30.*")]
-[assembly: AssemblyFileVersion("1.3.30.0")]
+[assembly: AssemblyVersion("1.3.31.*")]
+[assembly: AssemblyFileVersion("1.3.31.0")]
 
-[assembly: InternalsVisibleTo("QueueIT.Security.Tests")]
-[assembly: InternalsVisibleTo("QueueIT.Security.Mvc")]
+[assembly: InternalsVisibleTo("QueueIT.Security.Tests,PublicKey=002400000480000094000000060200000024000052534131000400000100010065de5b94ba442913157a4a38a4b6875e8caa61d5be2da5b9f0f48b052d4db5f17eb4715977946a8429dc41b12376a5e499ca935fe4bb77f0914e26773a36378fd58bb203070f03202852a14210de2bf836610a1e449bc996ea5484eb4be99a47caa10e0217a8578ce082eead1001c7645d86201d8c79d3f2209c9873aa51e48e")]
+[assembly: InternalsVisibleTo("QueueIT.Security.Mvc,PublicKey=002400000480000094000000060200000024000052534131000400000100010065de5b94ba442913157a4a38a4b6875e8caa61d5be2da5b9f0f48b052d4db5f17eb4715977946a8429dc41b12376a5e499ca935fe4bb77f0914e26773a36378fd58bb203070f03202852a14210de2bf836610a1e449bc996ea5484eb4be99a47caa10e0217a8578ce082eead1001c7645d86201d8c79d3f2209c9873aa51e48e")]
